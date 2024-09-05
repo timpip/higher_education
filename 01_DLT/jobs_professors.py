449b4d81b0458f668888ca6cbcf267ad435f41c7
vijisomu10 <viji.somu10@outlook.com>
2024-09-05 12:34:57 +0200

    dlt part

---
+++ 01_DLT/jobs_professors.py
@@ -0,0 +1,49 @@
+import dlt
+import requests
+import json
+from pathlib import Path
+import os
+
+def _get_ads(url_for_search, params):
+    headers = {"accept": "application/json"}
+    response = requests.get(url_for_search, headers=headers, params=params)
+    response.raise_for_status()  # check for http errors
+    return json.loads(response.content.decode("utf8"))
+
+
+@dlt.resource(write_disposition="replace")
+def jobsearch_resource(params):
+
+    url = "https://jobsearch.api.jobtechdev.se"
+    url_for_search = f"{url}/search"
+
+    for ad in _get_ads(url_for_search, params)["hits"]:
+        yield ad
+
+
+def run_pipeline(query, table_name):
+    pipeline = dlt.pipeline(
+        pipeline_name="jobsearch_he",
+        destination="snowflake",
+        dataset_name="staging",
+    )
+
+    params = {"q": query, "limit": 100}
+
+    load_info = pipeline.run(jobsearch_resource(params=params), table_name=table_name)
+    print(load_info)
+
+# högre utbildning (yrkeshögskolejobb, högskolejobb, universitetsjobb)
+# universitet
+# higher education (vocational college jobs, college jobs, university jobs
+if __name__ == "__main__":
+    working_directory = Path(__file__).parent
+    os.chdir(working_directory)
+
+    
+    query = "universitet"
+    table_name = "data_field_job_ads"
+
+
+
+    run_pipeline(query, table_name)
