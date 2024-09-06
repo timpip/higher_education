deb79d1d9a61cb8f9081d088dbb93d135bef2767
Ali B <alibajalan@hotmail.com>
2024-09-06 16:43:37 +0200

    Formar vissa kolumner enligt modelleringen

--- 03_DBT/dbt_code/models/src/src_e.sql
+++ 03_DBT/dbt_code/models/src/src_e.sql
@@ -5,7 +5,7 @@
 }}
 
 
-WITH stg_job_ads AS (SELECT * FROM {{ source('he_db', 'stg_data_ads') }})
+WITH stg_he AS (SELECT * FROM {{ source('he_db', 'stg_data_ads') }})
 
 SELECT *
-FROM stg_job_ads;
+FROM stg_he;
