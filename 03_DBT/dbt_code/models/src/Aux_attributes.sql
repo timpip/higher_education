c30d75e7047bc27fc30b72b2a159e3e7c7f97c48
Ali B <alibajalan@hotmail.com>
2024-09-09 17:16:19 +0200

    aux_attributes i src mappen

---
+++ 03_DBT/dbt_code/models/src/Aux_attributes.sql
@@ -0,0 +1,14 @@
+{{
+  config(
+    materialized = 'ephemeral',
+    )
+}}
+
+
+WITH stg_he AS (SELECT * FROM {{ source('he_db', 'stg_data_ads') }})
+
+SELECT -- id ska först, hittar inte just nu 
+    EXPERIENCE_REQUIRED as experience_required,
+    DRIVING_LICENSE_REQUIRED as driver_license,
+    ACCESS_TO_OWN_CAR as ACCESS_TO_CAR
+FROM stg_he
