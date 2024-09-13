189c92a53310bf8e1423ca89e62503e03c35ab51
Wuttichai Chueaklang <pete@mbpsomtuttichai.home>
2024-09-13 11:22:24 +0200

    wuttichai

---
+++ 04_STREAMLIT/connect_data_warehouse.py
@@ -0,0 +1,50 @@
+import os
+from dotenv import load_dotenv
+import streamlit as st
+import pandas as pd
+from snowflake.connector import connect  
+
+
+load_dotenv()
+
+query = "SELECT * FROM staging.data_field_job_ads"
+
+def query_job_listings(query):
+    try:
+        with connect(
+            user=os.getenv("SNOWFLAKE_USER"),
+            password=os.getenv("SNOWFLAKE_PASSWORD"),
+            account=os.getenv("SNOWFLAKE_ACCOUNT"),
+            warehouse=os.getenv("SNOWFLAKE_WAREHOUSE"),
+            database=os.getenv("SNOWFLAKE_DATABASE"),
+            schema=os.getenv("SNOWFLAKE_SCHEMA"),
+            role=os.getenv("SNOWFLAKE_ROLE"),
+        ) as conn:
+            df = pd.read_sql(query, conn)
+        return df
+    except Exception as e:
+        st.error(f"Ett fel uppstod vid anslutningen till Snowflake: {e}")
+        return None
+
+
+st.title("Jobbannonser Statistik - Arbetskollen")
+
+try:
+    
+    df = query_job_listings(query)
+
+    if df is not None:
+       
+        st.subheader("Jobbannonser")
+        st.dataframe(df)
+
+        
+        st.subheader("Antal annonser per ort")
+        city_counts = df['location'].value_counts()
+        st.bar_chart(city_counts)
+    else:
+        st.error("Ingen data kunde hämtas.")
+
+except Exception as e:
+    st.error(f"Ett oväntat fel uppstod: {e}")
+
